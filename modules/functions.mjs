94bb6c773348b82e4535d9d21847348ffa2fea12
CodeNameCoder1 <66467983+CodeNameCoder1@users.noreply.github.com>
2020-06-07 21:44:45 +0700

    Add files via upload

---
+++ modules/functions.mjs
@@ -0,0 +1,8 @@
+function notify(error) {
+    console.error("[!] Возникла ошибка. Если не понимаете в чем причина, свяжитесь со мной: https://vk.com/id233731786. Описывайте проблему сразу!");
+    console.error(error)
+}
+
+export {
+    notify
+}
